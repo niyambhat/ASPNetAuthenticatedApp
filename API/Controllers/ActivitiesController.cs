f561ab60addf8f68f911780a4a90625edd1e8962
Niyam Bhat <niyambhat@192-168-1-101.tpgi.com.au>
2023-06-06 15:19:46 +0800

    Skeleton

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace API.Controllers
+{
+	public class ActivitiesController : BaseApiController
+	{
+        private readonly DataContext _context;
+
+        public ActivitiesController (DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities() {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api/activities/1
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
+
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+	{
+       
+
+	}
+}
+
